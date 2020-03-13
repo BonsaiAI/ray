2ce0ed07300bc352ad2d65a2ee0f18b33f195139
Ruofan Kong <rukon@MININT-1TMTCFH.northamerica.corp.microsoft.com>
2020-03-12 17:33:51 -0700

    Fixed the flatten function.

--- python/ray/rllib/agents/sac/sac/rllib_proxy/_tf_model_v2.py
+++ python/ray/rllib/agents/sac/sac/rllib_proxy/_tf_model_v2.py
@@ -1,10 +1,22 @@
 import tensorflow as tf
-from tensorflow.layers import flatten
+import torch
 
-from ray.rllib.utils.annotations import PublicAPI
+from ray.rllib.utils.annotations import PublicAPI, DeveloperAPI
 from ray.rllib.agents.sac.sac.rllib_proxy._utils import executing_eagerly
 from ray.rllib.evaluation.sample_batch import SampleBatch
 from ray.rllib.models.model import restore_original_dimensions
+
+
+@DeveloperAPI
+def flatten(obs, framework):
+    """Flatten the given tensor."""
+    if framework == "tf":
+        return tf.layers.flatten(obs)
+    elif framework == "torch":
+        assert torch is not None
+        return torch.flatten(obs, start_dim=1)
+    else:
+        raise NotImplementedError("flatten", framework)
 
 
 @PublicAPI

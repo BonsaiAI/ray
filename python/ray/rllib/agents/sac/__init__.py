eaa166d78956fc6825258c63077d54c5081c68a9
Aditya Gudimella <aditya.gudimella@gmail.com>
2020-03-26 09:10:28 -0700

    Added SACTFPolicy to sac.__init__

--- python/ray/rllib/agents/sac/__init__.py
+++ python/ray/rllib/agents/sac/__init__.py
@@ -1,4 +1,5 @@
 from ray.rllib.agents.sac.config import DEFAULT_CONFIG
 from ray.rllib.agents.sac.trainer import SACTrainer
+from ray.rllib.agents.sac.sac_policy_graph import SACTFPolicy
 
-__all__ = ["DEFAULT_CONFIG", "SACTrainer"]
+__all__ = ["DEFAULT_CONFIG", "SACTrainer", "SACTFPolicy"]

9b61e4f49084938b13b0708b1fe91c6324f8b163
Aditya Gudimella <aditya.gudimella@gmail.com>
2020-08-07 17:14:37 -0700

    Bug fix for Apex SAC
    target_network_update was incorrectly set to > 0.
    SAC now learns on CartPole-v1.

--- rllib/agents/sac/apex.py
+++ rllib/agents/sac/apex.py
@@ -1,19 +1,19 @@
+"""ApexSAC Trainer and default config."""
+
 from ray.rllib.agents.dqn.apex import APEX_TRAINER_PROPERTIES
-from ray.rllib.agents.sac.sac import DEFAULT_CONFIG as SAC_CONFIG, SACTrainer
-from ray.rllib.utils import merge_dicts
+from ray.rllib.agents.sac.sac import SACTrainer, DEFAULT_CONFIG as SAC_CONFIG
 
 # yapf: disable
 # __sphinx_doc_begin__
 
-APEX_SAC_DEFAULT_CONFIG = merge_dicts(
-    SAC_CONFIG,  # see also the options in dqn.py, which are also supported
+APEX_SAC_DEFAULT_CONFIG = SACTrainer.merge_trainer_configs(
+    SAC_CONFIG,  # see also the options in sac.py, which are also supported
     {
-        "optimizer": merge_dicts(
-            SAC_CONFIG["optimizer"], {
-                "max_weight_sync_delay": 400,
-                "num_replay_buffer_shards": 4,
-                "debug": False
-            }),
+        "optimizer": {
+            "max_weight_sync_delay": 400,
+            "num_replay_buffer_shards": 4,
+            "debug": False,
+        },
         "n_step": 1,
         "num_gpus": 0,
         "num_workers": 32,
@@ -21,14 +21,14 @@ APEX_SAC_DEFAULT_CONFIG = merge_dicts(
         "learning_starts": 50000,
         "train_batch_size": 512,
         "rollout_fragment_length": 50,
-        "target_network_update_freq": 500000,
+        "target_network_update_freq": 0,
         "timesteps_per_iteration": 25000,
         "exploration_config": {"type": "PerWorkerEpsilonGreedy"},
         "worker_side_prioritization": True,
         "min_iter_time_s": 30,
+        "prioritized_replay": True,
     },
 )
-
 
 # __sphinx_doc_end__
 # yapf: enable
